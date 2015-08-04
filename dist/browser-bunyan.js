e9fdf3d4fee30e0ead501b95326b5515304a9ae2
Phil Mander <philip.mander@gmail.com>
2015-08-04 22:44:47 +0200

    Remove dependency on process

--- dist/browser-bunyan.js
+++ dist/browser-bunyan.js
@@ -1,5 +1,4 @@
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.bunyan = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-(function (process){
 /**
  * !This is a stripped down version of Bunyan targeted specifically for the browser
  *
@@ -137,7 +136,7 @@ function _warn(msg, dedupKey) {
         }
         _warned[dedupKey] = true;
     }
-    process.stderr.write(msg + '\n');
+    console.error(msg + '\n');
 }
 function _haveWarned(dedupKey) {
     return _warned[dedupKey];
@@ -407,9 +406,6 @@ function Logger(options, _childOptions, _childSimple) {
     delete fields.src;
     if (this.serializers) {
         this._applySerializers(fields);
-    }
-    if (!fields.pid) {
-        fields.pid = process.pid;
     }
     Object.keys(fields).forEach(function (k) {
         self.fields[k] = fields[k];
@@ -918,66 +914,5 @@ module.exports.safeCycles = safeCycles;
 //streams
 module.exports.ConsoleFormattedStream = ConsoleFormattedStream;
 module.exports.ConsoleRawStream = ConsoleRawStream;
-}).call(this,require('_process'))
-},{"_process":2}],2:[function(require,module,exports){
-// shim for using process in browser
-
-var process = module.exports = {};
-var queue = [];
-var draining = false;
-
-function drainQueue() {
-    if (draining) {
-        return;
-    }
-    draining = true;
-    var currentQueue;
-    var len = queue.length;
-    while(len) {
-        currentQueue = queue;
-        queue = [];
-        var i = -1;
-        while (++i < len) {
-            currentQueue[i]();
-        }
-        len = queue.length;
-    }
-    draining = false;
-}
-process.nextTick = function (fun) {
-    queue.push(fun);
-    if (!draining) {
-        setTimeout(drainQueue, 0);
-    }
-};
-
-process.title = 'browser';
-process.browser = true;
-process.env = {};
-process.argv = [];
-process.version = ''; // empty string to avoid regexp issues
-process.versions = {};
-
-function noop() {}
-
-process.on = noop;
-process.addListener = noop;
-process.once = noop;
-process.off = noop;
-process.removeListener = noop;
-process.removeAllListeners = noop;
-process.emit = noop;
-
-process.binding = function (name) {
-    throw new Error('process.binding is not supported');
-};
-
-// TODO(shtylman)
-process.cwd = function () { return '/' };
-process.chdir = function (dir) {
-    throw new Error('process.chdir is not supported');
-};
-process.umask = function() { return 0; };
-
 },{}]},{},[1])(1)
 });
