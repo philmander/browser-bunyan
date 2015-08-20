80033aed66bcb7f452af97dea6c98a24af9333e9
Phil Mander <philip.mander@gmail.com>
2015-08-20 13:16:44 +0200

    Updating meta and including in minified build Adding bower

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,9 @@ var uglify = require('gulp-uglify');
 var browserify = require('browserify');
 var vinylStream = require('vinyl-source-stream');
 var del = require('del');
+var util = require('util');
 var rename = require('gulp-rename');
+var insert = require('gulp-insert');
 
 gulp.task('clean', function () {
     return del('./dist/**/*.*');
@@ -31,10 +33,23 @@ gulp.task('package', [ 'lint', 'clean' ], function() {
 
 gulp.task('minify', [ 'package' ], function() {
 
+    var packageMessageTempalte =
+        '/**\n' +
+        ' * %s - %s\n' +
+        ' * %s\n' +
+        ' * %s\n' +
+        ' */\n';
+
+    var meta = require('./package.json');
+    var packageMessage = util.format(packageMessageTempalte, meta.name, meta.version, meta.description, meta.licenses[0].type);
 
     return gulp.src('./dist/browser-bunyan.js')
         .pipe(uglify())
-        .pipe(rename('browser-bunyan.min.js'))
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(uglify())
+        .pipe(insert.prepend(packageMessage))
         .pipe(gulp.dest('./dist'));
 });
 
