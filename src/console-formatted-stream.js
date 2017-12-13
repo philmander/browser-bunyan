60b174f277291eea9673201a41ab049f0d9c308a
Phil Mander <philip.mander@gmail.com>
2017-12-13 15:31:12 +0100

    feat: Make css for ConsoleFormattedStream customizable

--- src/console-formatted-stream.js
+++ src/console-formatted-stream.js
@@ -1,16 +1,30 @@
 import { TRACE, DEBUG, INFO, WARN, ERROR, FATAL, nameFromLevel } from './levels';
+const DEFAULT_CSS = {
+    levels : {
+        [TRACE] : 'color: DeepPink',
+        [DEBUG] : 'color: GoldenRod',
+        [INFO] : 'color: DarkTurquoise',
+        [WARN] : 'color: Purple',
+        [ERROR] : 'color: Crimson',
+        [FATAL] : 'color: Black',
+    },
+    def: 'color: DimGray',
+    msg : 'color: SteelBlue',
+    src : 'color: DimGray; font-style: italic; font-size: 0.9em',
+};
+
 export class ConsoleFormattedStream {
 
-    constructor({ logByLevel = false} = {}) {
-        this.logByLevel = !!logByLevel;
+    constructor({ logByLevel = false} = {}, css = DEFAULT_CSS) {
+        this.logByLevel = logByLevel;
+        this.css = css;
     }
 
     write(rec) {
-        let levelCss,
-            defaultCss = 'color: DimGray',
-            msgCss = 'color: SteelBlue',
-            srcCss = 'color: DimGray; font-style: italic; font-size: 0.9em',
-            consoleMethod;
+        let levelCss, consoleMethod;
+        const defaultCss = this.css.def;
+        const msgCss = this.css.msg;
+        const srcCss = this.css.src;
 
         const loggerName = rec.childName ? rec.name + '/' + rec.childName : rec.name;
 
@@ -30,17 +44,17 @@ export class ConsoleFormattedStream {
         }
 
         if (rec.level < DEBUG) {
-            levelCss = 'color: DeepPink';
+            levelCss = this.css.levels[TRACE];
         } else if (rec.level < INFO) {
-            levelCss = 'color: GoldenRod';
+            levelCss = this.css.levels[DEBUG];
         } else if (rec.level < WARN) {
-            levelCss = 'color: DarkTurquoise';
+            levelCss = this.css.levels[INFO];
         } else if (rec.level < ERROR) {
-            levelCss = 'color: Purple';
+            levelCss = this.css.levels[WARN];
         } else if (rec.level < FATAL) {
-            levelCss = 'color: Crimson';
+            levelCss = this.css.levels[ERROR];
         } else {
-            levelCss = 'color: Black';
+            levelCss = this.css.levels[FATAL];
         }
 
         const padZeros = (number, len) => Array((len + 1) - (number + '').length).join('0') + number;
