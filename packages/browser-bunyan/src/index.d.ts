a0b6ffec8fcf1f560c9401feebab3eaf92177a80
Simon Wienecke <s.wienecke@sternico.cloud>
2020-12-04 12:30:31 +0100

    fix: Improve type defnitions to match the actual js
    I'd like to propose a few changes to the type definitions of the main package. These would allow the package to better integrate in TypeScript projects.
    
    type LoggerOptions = { .... [key: string]: any; }
    
    Allows any additional properties to be added to the logged object as documented here in node-bunyan. As this also works for browser-buynan I added the respective documentation from node-bunyan.
    
    The definition for the first argument for Logger#child is wrong, as it doesn't expect LoggerOptions. Actually it expects LoggerOptions without the name property and an optional childName property and again any other additional properties. Here I added the type ChildLoggerOptions and referenced it as the type for the first argument of Logger#child
    
    Lastly I turned the interface for Logger into an exported class declaration, as it's actually a class that's exported from the module.
    
    These should not be a breaking changes.

--- packages/browser-bunyan/src/index.d.ts
+++ packages/browser-bunyan/src/index.d.ts
@@ -7,6 +7,17 @@ type LoggerOptions = {
     src?: boolean,
     stream?: LogStream,
     level?: string | number,
+    [key: string]: any
+}
+
+type ChildLoggerOptions = {
+    childName?: string
+    streams?: Array<StreamOptions>,
+    serializers?: Object,
+    src?: boolean,
+    stream?: LogStream,
+    level?: string | number,
+    [key: string]: any
 }
 
 type StreamOptions = {
@@ -14,53 +25,53 @@ type StreamOptions = {
     stream: LogStream,
 }
 
-interface Logger {
-    addStream(): void,
-    addSerializers(): void,
-    child(options?: LoggerOptions, simple?: boolean): Logger,
+export declare class Logger {
+    addStream(): void;
+    addSerializers(): void;
+    child(options?: ChildLoggerOptions, simple?: boolean): Logger;
 
-    level(level: string | number): void,
-    level(): number,
+    level(level: string | number): void;
+    level(): number;
 
-    levels(stream: string | number, level: string | number): void,
-    levels(stream: string): number,
-    levels(): Array<number>,
+    levels(stream: string | number, level: string | number): void;
+    levels(stream: string): number;
+    levels(): Array<number>;
 
     // trace
-    trace(fields: object, msg: string, ...args: any[]): void,
-    trace(err: Error, msg: string, ...args: any[]): void,
-    trace(msg: string, ...args: any[]): void,
-    trace(): boolean,
+    trace(fields: object, msg: string, ...args: any[]): void;
+    trace(err: Error, msg: string, ...args: any[]): void;
+    trace(msg: string, ...args: any[]): void;
+    trace(): boolean;
 
     // debug
-    debug(fields: object, msg: string, ...args: any[]): void,
-    debug(err: Error, msg: string, ...args: any[]): void,
-    debug(msg: string, ...args: any[]): void,
-    debug(): boolean,
+    debug(fields: object, msg: string, ...args: any[]): void;
+    debug(err: Error, msg: string, ...args: any[]): void;
+    debug(msg: string, ...args: any[]): void;
+    debug(): boolean;
 
     // info
-    info(fields: object, msg: string, ...args: any[]): void,
-    info(err: Error, msg: string, ...args: any[]): void,
-    info(msg: string, ...args: any[]): void,
-    info(): boolean,
+    info(fields: object, msg: string, ...args: any[]): void;
+    info(err: Error, msg: string, ...args: any[]): void;
+    info(msg: string, ...args: any[]): void;
+    info(): boolean;
 
     // warn
-    warn(fields: object, msg: string, ...args: any[]): void,
-    warn(err: Error, msg: string, ...args: any[]): void,
-    warn(msg: string, ...args: any[]): void,
-    warn(): boolean,
+    warn(fields: object, msg: string, ...args: any[]): void;
+    warn(err: Error, msg: string, ...args: any[]): void;
+    warn(msg: string, ...args: any[]): void;
+    warn(): boolean;
 
     // error
-    error(fields: object, msg: string, ...args: any[]): void,
-    error(err: Error, msg: string, ...args: any[]): void,
-    error(msg: string, ...args: any[]): void,
-    error(): boolean,
+    error(fields: object, msg: string, ...args: any[]): void;
+    error(err: Error, msg: string, ...args: any[]): void;
+    error(msg: string, ...args: any[]): void;
+    error(): boolean;
 
     // fatal
-    fatal(fields: object, msg: string, ...args: any[]): void,
-    fatal(err: Error, msg: string, ...args: any[]): void,
-    fatal(msg: string, ...args: any[]): void,
-    fatal(): boolean,
+    fatal(fields: object, msg: string, ...args: any[]): void;
+    fatal(err: Error, msg: string, ...args: any[]): void;
+    fatal(msg: string, ...args: any[]): void;
+    fatal(): boolean;
 }
 
 export type StdSerializers = {
